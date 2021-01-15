762e85d894d9f4bf562e4cd2a84f29eba67183cd
Silky Singh <54901747+silky1708@users.noreply.github.com>
2021-01-15 11:46:05 +0530

    Add files via upload

---
+++ Assignment 5/src/Scheduler Driver/Scheduler_driver.java
@@ -0,0 +1,208 @@
+package ProjectManagement;
+
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Scheduler_Driver extends Thread implements SchedulerInterface {
+
+
+    public static void main(String[] args) throws IOException {
+//
+
+        Scheduler_Driver scheduler_driver = new Scheduler_Driver();
+        File file;
+        if (args.length == 0) {
+            URL url = Scheduler_Driver.class.getResource("INP");
+            file = new File(url.getPath());
+        } else {
+            file = new File(args[0]);
+        }
+
+        scheduler_driver.execute(file);
+    }
+
+    public void execute(File commandFile) throws IOException {
+
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(commandFile));
+
+            String st;
+            while ((st = br.readLine()) != null) {
+                String[] cmd = st.split(" ");
+                if (cmd.length == 0) {
+                    System.err.println("Error parsing: " + st);
+                    return;
+                }
+                String project_name, user_name;
+                Integer start_time, end_time;
+
+                long qstart_time, qend_time;
+
+                switch (cmd[0]) {
+                    case "PROJECT":
+                        handle_project(cmd);
+                        break;
+                    case "JOB":
+                        handle_job(cmd);
+                        break;
+                    case "USER":
+                        handle_user(cmd[1]);
+                        break;
+                    case "QUERY":
+                        handle_query(cmd[1]);
+                        break;
+                    case "": // HANDLE EMPTY LINE
+                        handle_empty_line();
+                        break;
+                    case "ADD":
+                        handle_add(cmd);
+                        break;
+                    //--------- New Queries
+                    case "NEW_PROJECT":
+                    case "NEW_USER":
+                    case "NEW_PROJECTUSER":
+                    case "NEW_PRIORITY":
+                        timed_report(cmd);
+                        break;
+                    case "NEW_TOP":
+                        qstart_time = System.nanoTime();
+                        timed_top_consumer(Integer.parseInt(cmd[1]));
+                        qend_time = System.nanoTime();
+                        System.out.println("Time elapsed (ns): " + (qend_time - qstart_time));
+                        break;
+                    case "NEW_FLUSH":
+                        qstart_time = System.nanoTime();
+                        timed_flush( Integer.parseInt(cmd[1]));
+                        qend_time = System.nanoTime();
+                        System.out.println("Time elapsed (ns): " + (qend_time - qstart_time));
+                        break;
+                    default:
+                        System.err.println("Unknown command: " + cmd[0]);
+                }
+
+            }
+
+
+            run_to_completion();
+            print_stats();
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Input file Not found. " + commandFile.getAbsolutePath());
+        } catch (NullPointerException ne) {
+            ne.printStackTrace();
+
+        }
+    }
+
+    @Override
+    public ArrayList<JobReport_> timed_report(String[] cmd) {
+        long qstart_time, qend_time;
+        ArrayList<JobReport_> res = null;
+        switch (cmd[0]) {
+            case "NEW_PROJECT":
+                qstart_time = System.nanoTime();
+                res = handle_new_project(cmd);
+                qend_time = System.nanoTime();
+                System.out.println("Time elapsed (ns): " + (qend_time - qstart_time));
+                break;
+            case "NEW_USER":
+                qstart_time = System.nanoTime();
+                res = handle_new_user(cmd);
+                qend_time = System.nanoTime();
+                System.out.println("Time elapsed (ns): " + (qend_time - qstart_time));
+
+                break;
+            case "NEW_PROJECTUSER":
+                qstart_time = System.nanoTime();
+                res = handle_new_projectuser(cmd);
+                qend_time = System.nanoTime();
+                System.out.println("Time elapsed (ns): " + (qend_time - qstart_time));
+                break;
+            case "NEW_PRIORITY":
+                qstart_time = System.nanoTime();
+                res = handle_new_priority(cmd[1]);
+                qend_time = System.nanoTime();
+                System.out.println("Time elapsed (ns): " + (qend_time - qstart_time));
+                break;
+        }
+
+        return res;
+    }
+
+    @Override
+    public ArrayList<UserReport_> timed_top_consumer(int top) {
+        return null;
+    }
+
+
+
+    @Override
+    public void timed_flush(int waittime) {
+
+    }
+    
+
+    private ArrayList<JobReport_> handle_new_priority(String s) {
+        return null;
+    }
+
+    private ArrayList<JobReport_> handle_new_projectuser(String[] cmd) {
+        return null;
+    }
+
+    private ArrayList<JobReport_> handle_new_user(String[] cmd) {
+        return null;
+    }
+
+    private ArrayList<JobReport_> handle_new_project(String[] cmd) {
+        return null;
+    }
+
+
+
+
+    public void schedule() {
+            execute_a_job();
+    }
+
+    public void run_to_completion() {
+
+    }
+
+    public void print_stats() {
+
+    }
+
+    public void handle_add(String[] cmd) {
+
+    }
+
+    public void handle_empty_line() {
+       schedule();
+    }
+
+
+    public void handle_query(String key) {
+
+    }
+
+    public void handle_user(String name) {
+
+    }
+
+    public void handle_job(String[] cmd) {
+
+    }
+
+    public void handle_project(String[] cmd) {
+
+    }
+
+    public void execute_a_job() {
+
+    }
+}
